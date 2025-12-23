bf5fa02b0f6450a416d580f2e777082b53f29909
sourishdalvi <sourish.dalvi.19@gmail.com>
2025-12-23 16:22:12 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,9 @@
+a=5 
+print("Data Type of a is",type(a))
+b=True
+print("Data Type of b is",type(b))
+c="Sourish"
+print("Data Type of c is",type(c)) 
+d=5.849
+print("Data Type of c is",type(d)) 
+
