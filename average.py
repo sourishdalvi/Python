3f25127d9d637b766863d354654a2798540508d8
sourishdalvi <sourish.dalvi.19@gmail.com>
2025-12-24 16:41:59 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,10 @@
+Tree1=48
+Tree2=63
+Tree3=29
+tree4=67
+Tree5=128
+sum=Tree1+Tree2+Tree3+tree4+Tree5
+print("Height of all the trees combined is",sum)
+average=sum/5
+print("Average of the Height of the trees is",average)
+
