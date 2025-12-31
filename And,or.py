923c260b00bb391696ceef63dd99c7102be36dee
sourishdalvi <sourish.dalvi.19@gmail.com>
2025-12-31 16:46:00 +0530

    Add files via upload

---
+++ And,or.py
@@ -0,0 +1,14 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if a==b and b==c:
+ print("All numbers are equal to each other")
+else:
+ print("All number are not equal to each other")
+if a>0 or b>0 :
+ print("One of the numbers is greater than 0")
+else:
+ print("None of the numbers are greater than 0") 
+if b<0 or c<0:
+ print("one of the numbers is a negative number")
+
