5f93dcfbaa27a3ff3c248f5f732fc414be1eec34
sourishdalvi <sourish.dalvi.19@gmail.com>
2025-12-31 16:54:35 +0530

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+d=str(input("Enter a word"))
+e=str(input("Enter a word"))
+print(a!=b)
+print(b!=c)
+if d!=e:
+ print("both words different from each other")
+ 
+
