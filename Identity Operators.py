8b74a406893c70c84e511320e452fccb9dd0e6f2
Snxz <sourish.dalvi.19@gmail.com>
2026-01-06 16:32:47 +0530

    Add files via upload

---
+++ Identity Operators.py
@@ -0,0 +1,13 @@
+x= input("Enter a number")
+if (type(x) is int):
+    print("True")
+else:  
+    print("False")
+if (type(x) is float):
+    print("False")
+y= input("Enter a number")
+z= input("Enter a number")
+if (y is z):
+    print("Both numbers enterd are same")
+else:
+    print("Both numbers enterd are different")
