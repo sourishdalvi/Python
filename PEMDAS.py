a3f1b56b46c5363d222aff0577f18ba73e50f332
Snxz <sourish.dalvi.19@gmail.com>
2026-01-07 16:41:02 +0530

    Add files via upload

---
+++ PEMDAS.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+d=int(input("Enter a number"))
+print("The value of(a+b)*c-d is:",(a + b) * c - d)
+
+e=str(input("Enter Your Name"))
+f=int(input("Enter your age"))
+if e=="Sourish" or e=="SOURISH" or e=="sourish" and  f==13:
+ print("Password accepted")
