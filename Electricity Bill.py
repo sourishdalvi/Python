1267090330853776f693aa2a1f9fb749a443a772
Snxz <sourish.dalvi.19@gmail.com>
2026-01-13 16:40:06 +0530

    Add files via upload

---
+++ Electricity Bill.py
@@ -0,0 +1,9 @@
+u=float(input("Enter the units consumed: "))
+if u<=50:
+ bill=u*2.5+ u*0.5
+elif 50<u<=100:
+  bill=u*2.5+ u*0.5 
+elif 100<u<=200:
+  bill=u*2.5+ u*0.5
+print("The total electricity bill is: ",bill)
+
