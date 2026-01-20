caf71e6fcf7c973bf942b0273e86432a13a9d928
Snxz <sourish.dalvi.19@gmail.com>
2026-01-20 16:33:07 +0530

    Add files via upload

---
+++ Infinity.py
@@ -0,0 +1,7 @@
+i=int(input("Enter a negative number "))
+if i>0:
+    print("The number is not negative")
+else:
+    while i<0:
+        print("Its gonna be infinite and u cant stop it u lagged ur own computer")
+        
