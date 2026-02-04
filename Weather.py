d810473b2e079fd99712abda75fc3788c4181d36
Snxz <sourish.dalvi.19@gmail.com>
2026-02-04 16:53:29 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,6 @@
+def weather():
+ print("The weather in in India is generally hot and humis in",Spring)
+ print ("The weather in in India is same in",Autmn)
+Spring = "March, April, May"
+Autmn = Spring
+weather()
