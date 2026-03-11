045cf75888978e3fb8fd79774958a62a7ec00761
Snxz <sourish.dalvi.19@gmail.com>
2026-03-11 16:51:56 +0530

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,12 @@
+for x in range(10):
+ if x %20 == 0:
+  print("brizz")
+ elif x %10 == 0:
+    print("buzz")
+ elif x %5 == 0:
+  print("fizz") 
+ elif x %3 == 0: 
+  print("fuzz")
+ else:
+     print(x)
+
