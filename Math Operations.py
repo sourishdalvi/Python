36b7270b51dde4e9216131198740a99ab8da7a92
Snxz <sourish.dalvi.19@gmail.com>
2026-03-18 17:00:37 +0530

    Add files via upload

---
+++ Math Operations.py
@@ -0,0 +1,9 @@
+import math
+print('The floor and ceiling value of 23.56 are' +str(math.floor(23.56)) + ','+
+str(math.ceil(23.56)))
+
+x=10
+y=-15
+print('the value of x after copying the sign from y is' +str(math.copysign(x,y)))
+print('Absolute value of-96 and 56 are:' +str(math.fabs(-96)) + ' , ' + str(math.fabs(56)))
+print('The GCD of 24 and 56 is' +str(math.gcd(24,56)))
